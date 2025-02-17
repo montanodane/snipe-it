febcc89a528d034be462854b0a8a54e70a8bfc6e
montanodane <95035626+montanodane@users.noreply.github.com>
2025-02-17 16:05:10 +0000

    Update default.blade.php

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1158,13 +1158,7 @@ dir="{{ Helper::determineLanguageDirection() }}">
             </script>
         @endif
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script>
-$(document).ready(function() {
-    $('th:contains("Asset Name")').text('Sample');
-    $('th:contains("Device Image")').text('test');
-});
-</script>
+
 
 
                             
